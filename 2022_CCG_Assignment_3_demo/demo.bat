7fc33dbcd0f6fdf724eb9858f5e165029ebaf01c
duck-bongos <billmannd@gmail.com>
2022-10-28 16:52:29 -0400

    first commit

---
+++ 2022_CCG_Assignment_3_demo/demo.bat
@@ -0,0 +1,3 @@
+HodgeDecomposition.exe data\eight\eight.m data\eight\eight.open.m textures\checker_1k.bmp
+HodgeDecomposition.exe data\kitten\kitten.m data\kitten\kitten.open.m textures\coordinate_256.bmp
+HodgeDecomposition.exe data\torus\torus.m data\torus\torus.open.m textures\checker_1k.bmp
